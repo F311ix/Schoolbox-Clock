6c03db0b37df00fc2a31a2a57926e4362369f0ec
Felix <97383177+F311ix@users.noreply.github.com>
2023-11-09 11:05:48 +1300

    Fixed scaling issues, Fixed bugs

--- Schoolbox Clock/main.js
+++ Schoolbox Clock/main.js
@@ -1,10 +1,8 @@
-const collum = document.querySelector(".small-12.large-4.columns");
-
+const column = document.querySelector(".small-12.large-4.columns");
 
 
 function getPeriodLength(periodTime) {
-    const startTimeStr = periodTime.split("–")[0];
-    const endTimeStr = periodTime.split("–")[1];
+    const [startTimeStr, endTimeStr] = periodTime.split("–");
 
     const parseTime = (timeStr) => {
         const [hours, minutes] = timeStr.match(/\d+/g).map(Number);
@@ -26,12 +24,11 @@ function getPeriodLength(periodTime) {
     return Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
 }
 
-
 function getTimeToEnd(timeLeft) {
-
-    const targetTime = new Date();
     const [hour, minute] = timeLeft.match(/\d+/g).map(Number);
     const isPM = timeLeft.toLowerCase().includes("pm");
+
+    const targetTime = new Date();
     targetTime.setHours(isPM ? hour + 12 : hour);
     targetTime.setMinutes(minute);
     targetTime.setSeconds(0);
@@ -43,45 +40,40 @@ function getTimeToEnd(timeLeft) {
         const minutesRemaining = Math.floor((timeDifference % (60 * 60 * 1000)) / (60 * 1000));
         const secondsRemaining = Math.floor((timeDifference % (60 * 1000)) / 1000);
 
-        const timeRemainingText = minutesRemaining + " minutes, " + secondsRemaining + " seconds";
-
-        return timeRemainingText;
+        return `${minutesRemaining} minutes, ${secondsRemaining} seconds`;
     } else {
         return "Class finished";
     }
-
 }
 
 
-if (window.location.pathname === "/" && collum) {
+if (window.location.pathname === "/" && column) {
     const container = document.createElement("div");
-
-    let totalTime;
 
     let period;
     let periodTime;
-
     let progress;
+    let periodTextElement;
+    let periodInfoElement;
     let periodElement;
     let timetableElements;
-
 
     function createClock() {
         // Create frame title
         const h2 = document.createElement("h2");
         h2.classList.add("subheader");
-        h2.appendChild(document.createTextNode("Clock"));
+        h2.textContent = "Clock";
 
         // Create frame
         const frame = document.createElement("iframe");
         frame.src = "about:blank";
         frame.width = "100%";
-        frame.height = "264";
+        frame.height = "256";
 
         // Add frame to page
         container.appendChild(h2);
         container.appendChild(frame);
-        collum.insertBefore(container, collum.childNodes[0]);
+        column.insertBefore(container, column.firstChild);
 
         // Access document
         const iframeDocument = frame.contentDocument || frame.contentWindow.document;
@@ -91,50 +83,44 @@ if (window.location.pathname === "/" && collum) {
         // Add CSS to style the text
         const style = iframeDocument.createElement("style");
         style.innerHTML = `
-        h1 {
-            font-family: Roboto, sans-serif;
-            font-size: 32px;
-            font-weight: bold;
-            color: #303030;
-        }
+            h1 {
+                font-family: Roboto, sans-serif;
+                font-size: 32px;
+                color: #303030;
+            }
 
-        h2 {
-            font-family: Roboto, sans-serif;
-            font-size: 20px;
-            font-weight: bold;
-            color: #303030;
-        }
-
-        body {
-            font-family: Roboto, sans-serif;
-            font-size: 20px;
-            font-weight: normal;
-            color: #303030;
-        }
-    `;
+            h2, body {
+                font-family: Roboto, sans-serif;
+                font-size: 20px;
+                color: #303030;
+            }
+        `;
 
         iframeDocument.head.appendChild(style);
 
         // Header
         const header = iframeDocument.createElement("h1");
-        header.innerHTML = "Schoolbox Clock";
+        header.textContent = "Schoolbox Clock";
         iframeDocument.body.appendChild(header);
 
         // Add periods
-        periodElement = iframeDocument.createElement("h2");
-        iframeDocument.body.appendChild(periodElement);
+        periodTextElement = iframeDocument.createElement("h2");
+        iframeDocument.body.appendChild(periodTextElement);
 
         periodInfoElement = iframeDocument.createElement("p");
+        periodInfoElement.style.width = '98%';
         iframeDocument.body.appendChild(periodInfoElement);
 
-        // Add progress bar
+        // Add progress bar element
         const progressBar = iframeDocument.createElement('div');
         progressBar.style.width = '100%';
         progressBar.style.height = '25px';
-        progressBar.style.border = '3px solid #303030'; // 'solid' is the border style, 'black' is the border color
+        progressBar.style.border = '3px solid #303030';
         progressBar.style.borderRadius = '5px';
+        progressBar.style.backgroundColor = '#d9d9d9';
         iframeDocument.body.appendChild(progressBar);
 
+        // Add progrees nar
         progress = iframeDocument.createElement('div');
         progress.style.width = '0%';
         progress.style.height = '100%';
@@ -142,31 +128,31 @@ if (window.location.pathname === "/" && collum) {
         progressBar.appendChild(progress);
     }
 
+
     function updateClock() {
         timetableElements = document.getElementsByClassName("timetable-subject-active");
+        periodElement = document.getElementsByClassName("timetable-period-active");
 
         if (timetableElements.length > 0) {
-            let peroidInfoText;
+            let periodInfoText;
             if (timetableElements.length > 2) {
-                peroidInfoText = document.getElementsByClassName("timetable-period-active")[1].innerText;
+                periodInfoText = periodElement[1].innerText;
                 period = timetableElements[1].innerText.split("\n")[0];
-            }
-            else {
-                    peroidInfoText = document.getElementsByClassName("timetable-period-active")[0].innerText;
-                    period = timetableElements[0].innerText.split("\n")[0];
+            } else {
+                periodInfoText = periodElement[0].innerText;
+                period = timetableElements[0].innerText.split("\n")[0];
             }
 
-            periodNumber = peroidInfoText.split("\n")[0];
-            periodTime = peroidInfoText.split("\n")[1];
+            const periodNumber = periodInfoText.split("\n")[0];
+            periodTime = periodInfoText.split("\n")[1];
 
-            totalTime = getPeriodLength(periodTime);
             const timeLeft = getTimeToEnd(periodTime.split("–")[1]);
-            const timeLeftPer = ((Number(timeLeft.split(" minutes")[0])/totalTime)-1)*-100;
+            const timeLeftPer = ((Number(timeLeft.split(" minutes")[0]) / getPeriodLength(periodTime)) - 1) * -100;
 
-            progress.style.width = timeLeftPer+'%';
+            progress.style.width = timeLeftPer + '%';
 
-            periodElement.innerHTML = period;
-            periodInfoElement.innerHTML = '<b>' + periodNumber + '</b> (' + periodTime + ')' + ' will end in, <br> <b>' + timeLeft + '</b>';
+            periodTextElement.textContent = period;
+            periodInfoElement.innerHTML = `<b>${periodNumber}</b> (${periodTime}) will end in, <b>${timeLeft}</b>`;
         } else if (timetableElements.length === 0) {
             const simpleTime = new Date().toLocaleString('en-NZ', {
                 day: '2-digit',
@@ -177,11 +163,14 @@ if (window.location.pathname === "/" && collum) {
                 second: '2-digit',
             });
 
-            periodElement.innerHTML = 'No Classes';
-            periodInfoElement.style.fontSize = '20px'; 
+            periodTextElement.textContent = 'No Classes';
+            periodInfoElement.style.fontSize = '20px';
             periodInfoElement.innerHTML = simpleTime;
+
+            progress.style.width = '0px';
         }
     }
+
 
     createClock();
     setInterval(updateClock, 1000);
